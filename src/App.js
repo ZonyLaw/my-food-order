2f1bc134610b05af8bb453d70c9c1981aaaf6045
Zonylaw <sunny_law@hotmail.com>
2023-03-01 23:00:59 +0000

    get rid of order route as not used anymore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./route/home/home.component";
-// import { Directory } from "./components/directory/directory.component";
 import { Navigation } from "./route/navigation/navigation.component";
-import Order from "./route/order/order.component";
-// import RestaurantsPreview from "./components/restaurants-preview/restaurants-preview.components";
 import Restaurants from "./route/restaurants/restaurants.component";
 import Checkout from "./route/checkout/checkout.component";
 
@@ -18,7 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
-          <Route path="/order/*" element={<Order />} />
           <Route path="/restaurants/*" element={<Restaurants />} />
           <Route path="/checkout" element={<Checkout />} />
         </Route>
--- src/route/Order/order.component.jsx
+++
@@ -1,8 +0,0 @@
-// import { Routes, Route } from "react-router-dom";
-// import { Category } from "../../components/category/category.component";
-
-const Order = () => {
-  return <p> Order</p>;
-};
-
-export default Order;
